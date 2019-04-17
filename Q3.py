632da680b2cf17d6adc2b6b7a84ede320a24475d
msorce26 <sorcem0@students.rowan.edu>
2019-04-17 13:26:29 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -12,7 +12,7 @@ from keras.utils import np_utils
 from keras.datasets import cifar10
 from keras.regularizers import l2
 
-epochs = 40
+epochs = 20
 
 # Get the data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -31,16 +31,16 @@ input_img = Input(shape = (32, 32, 3))
 
 
 # Create Volumes for the Inception module
-volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+volume_1 = Conv2D(64, (1,1), padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01))(input_img)
 
 volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
-volume_2 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_2)
+volume_2 = Conv2D(128, (3,3), padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01))(volume_2)
 
 volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
-volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
+volume_3 = Conv2D(32, (5,5), padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01))(volume_3)
 
 volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
-volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
+volume_4 = Conv2D(32, (1,1), padding='same', activation='relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01))(volume_4)
 
 # Concatenate all volumes of the Inception module
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
