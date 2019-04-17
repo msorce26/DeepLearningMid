4640e3a8e056e4b6dfd9448eaeed069c7b48996e
msorce26 <sorcem0@students.rowan.edu>
2019-04-17 13:42:48 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -10,9 +10,8 @@ from keras.layers import Flatten, Dense
 from keras.layers import Input
 from keras.utils import np_utils
 from keras.datasets import cifar10
-from keras.regularizers import l2
 
-epochs = 20
+epochs = 100
 
 # Get the data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -31,22 +30,22 @@ input_img = Input(shape = (32, 32, 3))
 
 
 # Create Volumes for the Inception module
-volume_1 = Conv2D(64, (1,1), padding='same', activation='relu', kernel_regularizer=l2(0.1), bias_regularizer=l2(0.1))(input_img)
+volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
 
-volume_2 = Conv2D(96, (1,1), padding='same', activation='relu', kernel_regularizer=l2(0.1), bias_regularizer=l2(0.1))(input_img)
-volume_2 = Conv2D(128, (2,2), padding='same', activation='relu', kernel_regularizer=l2(0.1), bias_regularizer=l2(0.1))(volume_2)
+volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
+volume_2 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_2)
 
-volume_3 = Conv2D(16, (1,1), padding='same', activation='relu', kernel_regularizer=l2(0.1), bias_regularizer=l2(0.1))(input_img)
-volume_3 = Conv2D(32, (3,3), padding='same', activation='relu', kernel_regularizer=l2(0.1), bias_regularizer=l2(0.1))(volume_3)
+volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
+volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
 
-volume_4 = MaxPooling2D((2,2), strides=(1,1), padding='same')(input_img)
-volume_4 = Conv2D(32, (1,1), padding='same', activation='relu', kernel_regularizer=l2(0.1), bias_regularizer=l2(0.1))(volume_4)
+volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
+volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
 
 # Concatenate all volumes of the Inception module
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
                                              volume_4], axis = 3)
 output = Flatten()(inception_module)
-out    = Dense(10, activation='softmax', bias_regularizer=l2(0.05))(output)
+out    = Dense(10, activation='softmax')(output)
 
 
 model = Model(inputs = input_img, outputs = out)
