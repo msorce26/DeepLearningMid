6b40b53f4326d822bf21bee85a2d9bbaa1dc0a1d
msorce26 <sorcem0@students.rowan.edu>
2019-04-17 14:17:12 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -45,7 +45,41 @@ volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
                                              volume_4], axis = 3)
 
-output = Flatten()(inception_module)
+
+# Create Volumes for the Inception module
+volume_5 = Conv2D(64, (1,1), padding='same', activation='relu')(inception_module)
+
+volume_6 = Conv2D(96, (1,1), padding='same', activation='relu')(inception_module)
+volume_6 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_6)
+
+volume_7 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
+volume_7 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_7)
+
+volume_8 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
+volume_8 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_8)
+
+# Concatenate all volumes of the Inception module
+inception_module1 = keras.layers.concatenate([volume_5, volume_6, volume_7,
+                                             volume_8], axis = 3)
+
+# Create Volumes for the Inception module
+volume_9 = Conv2D(64, (1,1), padding='same', activation='relu')(inception_module1)
+
+volume_10 = Conv2D(96, (1,1), padding='same', activation='relu')(inception_module1)
+volume_10 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_10)
+
+volume_11 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
+volume_11 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_11)
+
+volume_12 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
+volume_12 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_12)
+
+# Concatenate all volumes of the Inception module
+inception_module2 = keras.layers.concatenate([volume_9, volume_10, volume_11,
+                                             volume_12], axis = 3)
+
+
+output = Flatten()(inception_module2)
 out    = Dense(10, activation='softmax')(output)
 
 
