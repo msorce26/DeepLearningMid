9d4e01a3d510f202991c8034b14053fce80ff331
msorce26 <sorcem0@students.rowan.edu>
2019-04-19 11:45:11 -0400

    Create Q5_Test.py

---
+++ Q5_Test.py
@@ -0,0 +1,38 @@
+"""
+PS#2
+Q5 (Testing) - A Large Character Level LSTM
+Loads a trained LSTM and mapping and generates sentences
+"""
+
+from pickle import load
+from keras.models import load_model
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+
+seed_text = 'All these things, and a thousand like them, came to pass in and close upon the dear old year one thousand'
+n_chars_to_predict = 500
+seq_length = 100
+
+# load the model and mapping
+model = load_model('model.h5')
+mapping = load(open('mapping.pkl', 'rb'))
+
+
+# Make predictions
+for k in range(n_chars_to_predict):
+    # encode the characters as integers
+    encoded = [mapping[char] for char in seed_text]
+    # truncate sequences to a fixed length
+    encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+    # one hot encode
+    encoded = to_categorical(encoded, num_classes=len(mapping))
+    # predict character
+    yhat = model.predict_classes(encoded, verbose=0)
+    
+    # reverse map integer to character
+    for char, index in mapping.items():
+        if index == yhat:
+            break
+    seed_text += char
+
+print(seed_text)
